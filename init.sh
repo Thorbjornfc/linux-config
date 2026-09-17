40e9d8065b0c685f057c96122edd3bb2a5ae481e
WSL external desktop <thorbjorn.christensen@aalborgportland.com>
2026-09-17 08:04:12 +0200

    wsl reverse search freeze on tab-to-complete fix and minor change in init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
-read -p "Warning! Continuing replaces current '.bashrc' and '.inputrc'. Press Y to continue, any other key to exit: " input
+read -n 1 -s -r -p "Warning! Continuing replaces current '.bashrc' and '.inputrc'. Press Y to continue, any other key to exit: " input
 if [[ "$input"!="y" && "$input"!="Y" ]]; then
+  echo ""
   exit 0
 fi
 
